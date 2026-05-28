4fd6a925ad073d7a8656f7b83713fbd465ee4d80
mangagroup <mangagroup712@gmail.com>
2026-05-28 11:38:39 +0900

    updated the domein

--- firebase-sync.js
+++ firebase-sync.js
@@ -21,7 +21,7 @@
     /* ── Firebase 設定 ──────────────────────────────────── */
     const FIREBASE_CONFIG = {
         apiKey: 'AIzaSyAmGyOqGPZOBGMQE739HKGnyda3-udubrc',
-        authDomain: 'logcal-60333.firebaseapp.com',
+        authDomain: 'logcal.f5.si',
         projectId: 'logcal-60333',
         storageBucket: 'logcal-60333.firebasestorage.app',
         messagingSenderId: '747132286989',
--- firebasemsg.js
+++ firebasemsg.js
@@ -1,6 +1,6 @@
 const PLANLY_FIREBASE_CONFIG = {
     apiKey: 'AIzaSyAmGyOqGPZOBGMQE739HKGnyda3-udubrc',
-    authDomain: 'logcal-60333.firebaseapp.com',
+    authDomain: 'logcal.f5.si',
     projectId: 'logcal-60333',
     storageBucket: 'logcal-60333.firebasestorage.app',
     messagingSenderId: '747132286989',
